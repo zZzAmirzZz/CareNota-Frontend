ceb0935e38acbdcc9a8a05051f268055a780bfdd
Amir <168018435+zZzAmirzZz@users.noreply.github.com>
2026-06-16 23:23:55 +0300

    Add production environment configuration

---
+++ CaraNota/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://carenota-hfcfbreqfeemf8dr.polandcentral-01.azurewebsites.net',
+  aiUrl: 'https://carenota-ai.calmsand-7a404348.polandcentral.azurecontainerapps.io/'
+};
